ed01da41306f7ec3b8c4f96f9150428205c285cd
dmitry <dmitry.orda@gmail.com>
2022-09-20 01:07:54 +0300

    создал базу данных и сделал одну запись

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tort.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article &r>' % self.id
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/create_article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form["title"]
+        intro = request.form["intro"]
+        text = request.form["text"]
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+
+        except:
+            return "При добавлении статьи произошла ошибка"
+
+    else:
+        return render_template('create_article.html')
 
 
 @app.route('/about')
