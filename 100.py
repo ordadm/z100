e82636765997d7464394038a3129abdadf48c002
dmitry <dmitry.orda@gmail.com>
2022-09-22 01:37:42 +0300

    рабочий алгоритм про заключенных. все красиво

--- 100.py
+++ 100.py
@@ -15,15 +15,10 @@
 
 import random
 
-# определяем количество заключенных
-# qtyprisoner:int=int(input("Введите количество заключенных: "))
-qtyprisoner: int = 100
-qtymissions: int = 100000
-qtychance: int = 50
-
-
-# #определяем количество экспериментов
-# qtyexperiment:int=int(input("Введите количество экспериментов (не менее 100): "))
+qtyprisoner: int = 10000      # количество заключенных
+qtymissions: int = 10**2    # количество экспериментов
+qtychance: int = 5000       # количество коробочек которые можно открыть
+positiv = 0                 # количество успешных экспериментов
 
 # создание комнаты
 def makeroom() -> dict:
@@ -40,22 +35,22 @@ def makeroom() -> dict:
         droom.setdefault(i + 1, int(lroom[i]))
     return droom
 
-
-def openfirstbox(room, number_prisoner):
+# открытие первой коробочки по своему номеру
+def openfirstbox(room, number_prisoner) -> int:
     secondbox = room.get(number_prisoner)
     return secondbox
 
-
-def opennextbox(room, nextbox):
+# открытие следующей коробочки
+def opennextbox(room, nextbox) -> int:
     box = room.get(nextbox)
     return box
 
-
-def wayprisoner(room, prisoner):
+# результат прохождения комнаты заключенным
+def wayprisoner(room, prisoner) -> bool:
     nextbox = openfirstbox(aroom, prisoner)
     if nextbox == prisoner:
         chance = 1
-        print('заключенный №', prisoner, 'c ', chance, ' попытки')
+#       print('заключенный №', prisoner, 'c ', chance, ' попытки')
         return True
     else:
         chance = 2
@@ -63,50 +58,32 @@ def wayprisoner(room, prisoner):
         while chance <= qtychance:
 
             if nextbox == prisoner:
-                print('заключенный №', prisoner, 'c ', chance, ' попытки')
+#               print('заключенный №', prisoner, 'c ', chance, ' попытки')
                 return True
             chance += 1
             nextbox = opennextbox(room, nextbox)
         return False
 
-#aroom={1: 9, 2: 6, 3: 7, 4: 4, 5: 8, 6: 3, 7: 5, 8: 10, 9: 1, 10: 2}
-
-
-
-# for pris in range(1,qtyprisoner+1):
-#
-#     print(wayprisoner(aroom,pris))
-
-# for pris in range(1, qtyprisoner + 1):
-#     print(f'Заключенный {pris} Шанс1')
-#     nextbox = openfirstbox(aroom, pris)
-#     print(f'{pris} -> {nextbox}')
-#     chance = 2
-#     for c in range(chance, qtychance+1):
-#         print(f'Заключенный {pris} Шанс,{c}')
-#         print(f'{nextbox} -> {opennextbox(aroom, nextbox)}')
-#         nextbox = opennextbox(aroom, nextbox)
-
-
-
-def mission(room):
+# результат прохождения комнаты всеми заключенными
+def mission(room) -> bool:
     for np in range(0, qtyprisoner):  # выбираем заключенных по одному
         number_prisoner = np + 1
         if wayprisoner(aroom, number_prisoner) == False:
             return False
-
     return True
 
-positiv = 0
-
+# отработка эксперимента несколько раз для статистики
 for stat in range(1, qtymissions+1):
     aroom = makeroom()
-    print(f'миссия №, {stat}, стартовала')
-    print(aroom)
-
-    if mission(aroom) == True:
-        print(f'миссия№ {stat} успешна')
+#    print(f'миссия №, {stat}, стартовала')
+#    print(aroom)
+    if mission(aroom):
         positiv += 1
-    else:
-        print ('миссия №',stat,'провалена')
-print(f'Пройденых миссий {positiv}')
+#       print(f'миссия№ {stat} успешна')
+#    else:
+#        print ('миссия №',stat,'провалена')
+
+# Отображение результатов
+print(f'Количество заключенных {qtyprisoner}, количество попыток (открытие коробочек) {qtychance} \n'
+      f'Пройденых миссий {positiv} из {qtymissions}, что является {round(positiv/qtymissions*100,4)}%')
+
